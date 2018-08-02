d8810aa98cd4239ff8c500c79abe836b065adc41
yirank <kangyiran1996@gmail.com>
2018-08-02 18:34:18 +0800

    more solutions: bst - 162. Find Peak Element

--- 162 Find Peak Element.cpp
+++ 162 Find Peak Element.cpp
@@ -24,4 +24,30 @@ public:
     }
 };
 
+// solution 2:
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        if (nums.size() == 1) return 0;
+        if (nums[1] < nums[0]) return 0;
+        if (nums[nums.size()-2] < nums[nums.size()-1]) return nums.size()-1;
+        
+        int k = nums.size() / 2;
+        int low = 0, high = nums.size()-1;
+        while (low < high) {
+            int mid = (low + high) / 2; // the front middle
+            if (nums[mid] < nums[mid+1]) {
+                low = mid+1;
+            } else {
+                high = mid;
+            }
+        }
+        
+        return low;
+    }
+};
 
+// this problem is the equivalent to find the local maximum in the vector by using binary search.
+// the most delicate solution is that every binary search, compare the middle two numbers to pick the larger one,
+// who is more likely to be the local maximum, so then we choose the interval who includes the larger one to binary search again.
+ 

d3d627846eaa0416bdd950c208cffd90ee63c386
yirank <kangyiran1996@gmail.com>
2018-08-21 13:12:40 +0800

    [refine]string: 5 Longest Palindromic Substring

--- 5 Longest Palindromic Substring.cpp
+++ 5 Longest Palindromic Substring.cpp
@@ -33,3 +33,37 @@ public:
 // 2. Input: "abb", Output: "a", Expected: "bb"
 
 // TIME LIMIT EXCEED!!! 
+
+
+// DP
+class Solution {
+public:
+
+    string longestPalindrome(string s) {
+        queue<pair<int,int>> tovisit;
+        for (int i = 0; i < s.length(); i++) {
+            int st = i;
+            while (i < s.length()-1 && s[i] == s[i+1]) i++;
+            tovisit.push(make_pair(st, i));
+        }
+        
+        int longest = 0, st = 0;
+        while (!tovisit.empty()) {
+            pair<int, int> interval = tovisit.front();
+            tovisit.pop();
+            int i = interval.first, j = interval.second;
+            if (j - i + 1 > longest) {
+                longest = j-i+1;
+                st = i;
+            }
+            
+            if (i <= j && i-1 >= 0 && j+1 < s.length() && s[i-1] == s[j+1]) {
+                tovisit.push(make_pair(i-1, j+1));
+            }
+        }
+        
+        return s.substr(st, longest);
+    }
+};
+
+

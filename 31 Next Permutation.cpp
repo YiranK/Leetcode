d5ecfd21b1c87deecd5490452f4b9093cf6afdcd
yirank <kangyiran1996@gmail.com>
2018-09-27 23:40:48 -0500

    31:refine

--- 31 Next Permutation.cpp
+++ 31 Next Permutation.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+    // no need for this function, because you just scan from right to left,
+    // use while loop to stop, when you meet the requirement of nums[i] < nums[i+1]
+    // (notice that there is no "=", because you need to find the strict larger one permutation)
     bool strictDesc(vector<int>& nums, int low) {
         for (int i = low; i < nums.size()-1; i++) {
             if (nums[i] < nums[i+1]) return false;
@@ -35,5 +38,40 @@ public:
             sort(nums.begin(), nums.end());
         }
         
+    }
+};
+
+
+class Solution {
+public:
+    void swap(vector<int>& nums, int low, int high) {
+        int tmp = nums[low];
+        nums[low] = nums[high];
+        nums[high] = tmp;
+    }
+    
+    void reverse(vector<int>& nums, int low) {
+        int high = nums.size()-1;
+        while (low < high) {
+            swap(nums, low++, high--);
+        }
+    }
+    
+    void nextPermutation(vector<int>& nums) {
+        int i = nums.size()-2;
+        while (i >= 0 && nums[i] >= nums[i+1]) i--;
+        // if the whole vector is passed, after the while loop, `i` becomes -1.
+        // This situation means the vector is in desc order.
+
+        if (i < 0) {
+            reverse(nums, 0);
+        } else {
+            // notice that the right section is in desc order, so no need to sort them, just simply reverse them to ascen order
+            reverse(nums, i+1);
+            int j = i+1;
+            // look up for the first element larger than nums[i], in ascen order
+            while (j < nums.size() && nums[j] <= nums[i]) j++;
+            swap(nums, i, j);
+        }
     }
 };
